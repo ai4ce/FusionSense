700900cba628dcfc919c4b43ed51df2f679cfa70
Irving Fang <zichuanfang2015@yahoo.com>
2024-09-07 22:36:19 -0400

    initial push for real world code

---
+++ fusion_sense/touch.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from fusion_sense.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'fusion_sense'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='irving',
+    maintainer_email='zichuanfang2015@yahoo.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'touch = fusion_sense.touch:main'
+        ],
+    },
+)
