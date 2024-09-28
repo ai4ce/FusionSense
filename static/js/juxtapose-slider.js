2f769a32497a6dac4bed8070e78bbafcd2e7b2c1
IrvingF7 <zichuanfang2015@yahoo.com>
2024-09-28 18:08:20 -0400

    now the sliders live locally. Need to fill in the textual content

---
+++ static/js/juxtapose-slider.js
@@ -0,0 +1,19 @@
+function createSlider(elementId, images, options) {
+  console.log(`Creating slider for ${elementId} with images:`, images); // Debugging line
+  return new juxtapose.JXSlider(elementId, images, options);
+}
+
+const sliderOptions = {
+  animate: true,
+  showLabels: true,
+  showCredits: false,
+  startingPosition: "35%",
+  makeResponsive: true
+};
+
+// Function to initialize sliders with image data
+function initializeSliders(sliders) {
+  sliders.forEach(slider => {
+    createSlider(slider.elementId, slider.images, slider.options);
+  });
+}
